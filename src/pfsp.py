0e497dc66c681b489ecebca5ceb72b6b734418ad
vsgirelli <vavi_sg@hotmail.com>
2019-06-18 00:24:55 -0300

    Parsing the input

--- src/pfsp.py
+++ src/pfsp.py
@@ -1,16 +1,33 @@
-import sys, getopt
+import sys
 
-seed = ''
-inst = ''
-try:
-    opts, args = getopt.getopt(sys.argv, "f:s", ["seed=","file="])
-except getopt.GetoptError:
-    print("psfp.py -s <seed> -f <instance_file>")
-    sys.exit(2)
-for opt, arg in opts:
-    if opt in ('-s', "--seed"):
-        seed = arg
-    elif opt in ('-f', "--file"):
-        inst = arg
-print(seed)
-print(inst)
+if len(sys.argv) < 2:
+    print("Usage: python3 pfsp.py inputFile")
+    sys.exit(1)
+
+def read_values(filename):
+    with open('../inputs/' + filename) as f:
+        return [int(elem) for elem in f.read().split()]
+
+def read_input():
+    inputFile = iter(read_values(sys.argv[1]))
+    # number of jobs and machines
+    nbtasks = int(next(inputFile))
+    nbmachines = int(next(inputFile))
+
+    # i-th job's processing time at j-th machine
+    processingTime = []
+
+    for i in range(nbtasks):
+        temp = []
+        for j in range(nbmachines):
+            time = int(next(inputFile))
+            time = int(next(inputFile))
+            temp.append(time)
+        processingTime.append(temp)
+
+    print(nbtasks)
+    print(nbmachines)
+    print(processingTime)
+
+
+read_input()
