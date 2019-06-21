9d2ea8ff6aed5eb55c456210e0a72155d442ab08
vsgirelli <vavi_sg@hotmail.com>
2019-06-21 19:02:39 -0300

    Trying to change how to initialize and read files

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+import sys
+sys.path.insert(0, './src/')
+import pfsp
+
+def readValues(filename):
+    with open(filename) as f:
+        return [int(elem) for elem in f.read().split()]
+
+def readInput():
+    inputFile = iter(readValues(sys.argv[1]))
+    nbtasks = int(next(inputFile))
+    nbmachines = int(next(inputFile))
+    seed = sys.argv[2]
+    processingTimes = []
+
+    for i in range(nbtasks):
+        temp = []
+        for j in range(nbmachines):
+            # this line is only to skip the machine's indicators in the inputFile
+            time = int(next(inputFile))
+            # the only thing we're interested about is the execution time
+            time = int(next(inputFile))
+            temp.append(time)
+        processingTimes.append(temp)
+
+    #print(nbtasks)
+    #print(nbmachines)
+    #print(processingTimes)
+    #print(seed)
+
+    return nbtasks, nbmachines, processingTimes, seed
+
+
+if len(sys.argv) < 3:
+    p.pprint("Usage: python3 pfsp.py inputFile seed")
+    sys.exit(1)
+        
+nbtasks, nbmachines, processingTimes, seed = readInput()
+pfsp.main(nbtasks, nbmachines, processingTimes, seed)
--- src/pfsp.py
+++ src/pfsp.py
@@ -3,49 +3,7 @@ import sys
 import random
 import numpy as np
 import pprint as p
-
-# number of jobs and machines
-nbtasks = 0
-nbmachines = 0
-# i-th job's processing time at j-th machine
-processingTimes = []
-seed = 0
-
-# For the SA, we need to keep track of the current and the old best solutions.
-# They'll only represent the order in which the tasks should be executed.
-# Ex: oldbest = [9, 3, 4, 1, 6, 2, 7, 0, 8, 5]
-# is how the old best solution sheduled the tasks.
-# When calculating the makespan, we're going to verify the execution times
-# stored in processingTimes.
-oldbest = []
-newbest = []
-
-def readValues(filename):
-    with open(filename) as f:
-        return [int(elem) for elem in f.read().split()]
-
-def readInput():
-    inputFile = iter(readValues(sys.argv[1]))
-    nbtasks = int(next(inputFile))
-    nbmachines = int(next(inputFile))
-    seed = sys.argv[2]
-
-    for i in range(nbtasks):
-        temp = []
-        for j in range(nbmachines):
-            # this line is only to skip the machine's indicators in the inputFile
-            time = int(next(inputFile))
-            # the only thing we're interested about is the execution time
-            time = int(next(inputFile))
-            temp.append(time)
-        processingTimes.append(temp)
-
-    #print(nbtasks)
-    #print(nbmachines)
-    #print(processingTimes)
-    #print(seed)
-
-    return nbtasks, nbmachines, processingTimes, seed
+from sa import *
 
 # The initial random solution
 def randomNeighboor(nbtasks, seed):
@@ -84,44 +42,57 @@ def calcMakespan(sol, processingTimes, nbtasks, nbmachines):
 # In the second iteration, the second smaller mean execution time will be swaped
 # with the second task to be executed in the given solution, and so on.
 def newNeighboor(sol, processingTimes, iteration):
-    print("Initial random solution")
-    p.pprint(sol)
     # just generating a numpy array with the processingTimes
     means = np.array(processingTimes)
     # taking the mean execution time for each task
     means = np.mean(means, axis = 1)
+    #print("Means:")
+    #p.pprint(means)
 
-    print("Means:")
-    p.pprint(means)
-
+    # ordering the means array and saving the indexes ordered to swap
     indexes = np.argsort(means)
-    print("indexes sorted")
-    p.pprint(indexes)
+    #print("indexes sorted")
+    #p.pprint(indexes)
 
     # swaps the iteration'th element of the solution with the
     # iteration'th smaller mean execution time
     sol[iteration], sol[indexes[iteration]] = sol[indexes[iteration]], sol[iteration]
-    print("new swaped solution")
-    p.pprint(sol)
     return sol
 
 # EXECUTION
-if len(sys.argv) < 3:
-    p.pprint("Usage: python3 pfsp.py inputFile seed")
-    sys.exit(1)
+def main(tasks, machines, times, iseed):
+    # number of jobs and machines
+    nbtasks = tasks
+    nbmachines = machines
+    # i-th job's processing time at j-th machine
+    processingTimes = times
+    seed = iseed
 
-# reads the input and initializes the variables
-nbtasks, nbmachines, processingTimes, seed = readInput()
+    # For the SA, we need to keep track of the current and the old best solutions.
+    # They'll only represent the order in which the tasks should be executed.
+    # Ex: oldbest = [9, 3, 4, 1, 6, 2, 7, 0, 8, 5]
+    # is how the old best solution sheduled the tasks.
+    # When calculating the makespan, we're going to verify the execution times
+    # stored in processingTimes.
+    oldbest = []
+    newbest = []
 
-#p.pprint(processingTimes)
-# first random solution
-solution = randomNeighboor(nbtasks, seed)
-# calculating the makespan
-makespan = calcMakespan(solution, processingTimes, nbtasks, nbmachines)
-#p.pprint(makespan)
-# generating a better solution
-for iteration in range(0,(nbtasks//2)):
-    solution = newNeighboor(solution, processingTimes, iteration)
+    # reads the input and initializes the variables
+
+    #p.pprint(processingTimes)
+    # first random solution
+    solution = randomNeighboor(nbtasks, seed)
+    # calculating the makespan
     makespan = calcMakespan(solution, processingTimes, nbtasks, nbmachines)
-    print("Makespan:")
-    print(makespan)
+    #p.pprint(makespan)
+    # generating a better solution
+    for iteration in range(0,(nbtasks//2)):
+        print("Initial random solution")
+        p.pprint(solution)
+        solution = newNeighboor(solution, processingTimes, iteration)
+        print("new swaped solution")
+        p.pprint(solution)
+        makespan = calcMakespan(solution, processingTimes, nbtasks, nbmachines)
+        print("Makespan:")
+        print(makespan)
+
